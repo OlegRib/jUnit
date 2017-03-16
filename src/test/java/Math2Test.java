fecdf72474bf0840c972f914a26163534d923c3e
oleg <oleg.ribchinskyy@gmail.com>
2017-03-16 21:12:54 +0200

    Add one unit test for min() of Math

--- src/test/java/Math2Test.java
+++ src/test/java/Math2Test.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by Zazonga on 16.03.2017.
@@ -9,7 +10,10 @@ public class Math2Test {
     @Test
     public void max() throws Exception {
     Math2 math = new Math2();
-    assertEquals(5, math.max(1,4));
+    assertEquals(5, math.max(1,5));
     }
-
+    @Test
+    public void min() {
+        assertTrue(3 > Math.min(1,2));
+    }
 }
